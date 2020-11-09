5ca70e233e5942afdda168c7286ef5645dfafa0a
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-11-09 13:31:43 -0600

    added add to savings

--- buisness_logic.py
+++ buisness_logic.py
@@ -1,10 +1,11 @@
 class Account:
-    def _init_(self):
+    def __init__(self):
         self.balance = 0  
+        self.savings= 0
         print('Your Account is Created.')
     def deposit(self):
         amount=int(input('Enter the amount to deposit: '))
-        self.balance = amount
+        self.balance += amount
         print('Your New Balance= %d' %self.balance)
     def withdraw(self):
         amount=int(input('Enter the amount to withdraw: '))
@@ -14,7 +15,13 @@ class Account:
             self.balance-=amount
     def enquiry(self):
         print('Your Balance = %d' %self.balance)
+    def add_to_savings(self):
+        number=int(input("How much do you want to transfer to Savings Account?"))
+        if(number>self.balance):
+            print('Insufficient Balance!')
+        else:
+            self.balance-=number
+            self.savings +=number
+            print('Your New Savings Balance= %d' %self.savings)
 
-    # def balance(self):
-    #     return(self.balance)
 account = Account()
