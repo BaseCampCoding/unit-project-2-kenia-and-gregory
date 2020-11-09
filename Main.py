ead7ce9467b573b3f3d1fec59578f53be5988033
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-11-09 16:15:14 -0600

    stuff

--- Main.py
+++ Main.py
@@ -1,30 +1,31 @@
 import buisness_logic as bl
+
 print('"""""""""""""""""""""""""""""')
 print("WELCOME TO BANK OF BASE CAMP!")
 print('"""""""""""""""""""""""""""""')
 
 while True:
-    answer=input(
-    """What do you want to do?
+    answer = input(
+        """What do you want to do?
     -View Checkings Account
     -View Savings Account
     -Deposit
     -Withdraw
     -QUIT
-    > """)
-    if answer == 'Check Account'.lower():
-       bl.account.enquiry()
+    > """
+    )
+    if answer == "Check Account".lower():
+        bl.account.enquiry()
 
     elif answer == "Deposit".lower():
         bl.account.deposit()
 
     elif answer == "Withdraw".lower():
         bl.account.withdraw()
-    
+
     elif answer == "Check Savings".lower():
         bl.account.add_to_savings()
-        
+
     elif answer == "Quit".lower():
         print("Goodbye")
         break
-    
