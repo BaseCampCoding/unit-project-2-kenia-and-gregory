15d168cb56a917d1805583f24a731e58a77e391b
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-11-11 09:37:55 -0600

    changed input to numbers

--- Main.py
+++ Main.py
@@ -9,35 +9,36 @@ while True:
     while True:
         answer = input(
             """What do you want to do ?
-        -Checkings Account
-        -Savings Account
-        -Deposit
-        -Withdraw
-        -Transfer
-        -Set up a Budget
-        -EXIT
+        -[1] Checkings Account
+        -[2] Savings Account
+        -[3] Deposit
+        -[4] Withdraw
+        -[5] Transfer
+        -[6] Set up a Budget
+        -[7] View Budget 
+        -[8] EXIT
         > """)
-        if answer == 'Check Account'.lower():
+        if answer == '1':
             bl.account.enquiry()
         
-        elif answer == 'Check Savings'.lower():
+        elif answer == '2':
             bl.account.Savings_enquiry()
 
-        elif answer == 'Deposit'.lower():
+        elif answer == '3':
             bl.account.deposit()
 
-        elif answer == 'Withdraw'.lower():
+        elif answer == '4':
             bl.account.withdraw()
 
-        elif answer == "Check Savings".lower():
-            bl.account.Savings_enquiry()
-            
-        elif answer == 'Transfer'.lower():
+        elif answer == '5':
             bl.account.add_to_savings()
 
-        elif answer == "Budget".lower():
+        elif answer == "6":
             bl.account.budgets()
+        
+        elif answer == "7":
+            bl.account.view_budget()
 
-        elif answer == "Exit".lower():
+        elif answer == "8":
             print("Goodbye")
             break
